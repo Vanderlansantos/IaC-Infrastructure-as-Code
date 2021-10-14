6c6ab688ec790a6cc80072ee9be7c5a91e732651
vanderlan carvalho <vanderlan_go2010@hotmail.com>
2021-10-14 00:58:01 -0300

    AKS Azure

---
+++ AKS - terraform/AKS - terraform - local/main.tf
@@ -0,0 +1,33 @@
+provider "azurerm" {
+    version = "=2.78.0"
+    features {}
+}
+
+terraform {
+  backend "local" {}
+}
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "resource_group_terraform" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "azure_aksvanderlan01" {
+  name = var.aks
+  location = azurerm_resource_group.resource_group_terraform.location
+  resource_group_name = azurerm_resource_group.resource_group_terraform.name
+  dns_prefix          = "aks123"
+
+  default_node_pool {
+    name = "default"
+    node_count = 1
+    vm_size = "Standard_a2_v2"
+  }
+  
+  identity {
+    type = "SystemAssigned"
+  }
+
+
+}
