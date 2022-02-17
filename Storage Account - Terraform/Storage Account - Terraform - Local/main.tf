ae3b9ecc6bd3afa12ab1ef25abab7230216efcae
vanderlan carvalho <vanderlan_go2010@hotmail.com>
2022-02-16 21:51:05 -0300

    Storage Account

---
+++ Storage Account - Terraform/Storage Account - Terraform - Local/main.tf
@@ -0,0 +1,24 @@
+provider "azurerm" {
+    version = ">=2.68.0"
+    features {}
+}
+
+terraform {
+  backend "local" {}
+}
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "resource_group" {
+  name = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "resource_storage" {
+    name = var.storage_account_name
+    resource_group_name = azurerm_resource_group.resource_group.name
+    location = azurerm_resource_group.resource_group.location
+    account_tier = "Standard"
+    account_replication_type = "LRS"
+}
+
+
---
+++ Storage Account - Terraform/Storage Account - Terraform - Local/variable.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+    default     = "dev"
+    description  = "Resourcer Group Azure"
+}
+
+variable "location"  {
+  default     = "eastus"
+  description = "Location Azure"
+}
+
+
+variable "storage_account_name"{
+    default = "storageteste0101"
+    description = "Nome do storage account"
+}
