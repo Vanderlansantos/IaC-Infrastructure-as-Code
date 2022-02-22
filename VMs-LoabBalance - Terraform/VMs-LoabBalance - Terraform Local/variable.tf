1a368238663e73e5c00c28837701afc452aae62c
vanderlan santos <vanderlan_go2010@hotmail.com>
2022-02-22 00:17:08 -0300

    Update variable.tf

--- VMs-LoabBalance - Terraform/VMs-LoabBalance - Terraform Local/variable.tf
+++ VMs-LoabBalance - Terraform/VMs-LoabBalance - Terraform Local/variable.tf
@@ -2,7 +2,6 @@ variable "resource_group_name" {
     default     = "dev"
     description  = "Resourcer Group Azure"
 }
-
 variable "location"  {
   default     = "eastus"
   description = "Location Azure"
